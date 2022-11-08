1e8282064053d9dbcbd756d3f28aee1530cabdf5
ammoriddin <hakimjavoh@gmail.com>
2022-11-08 13:58:34 +0600

    Finish

---
+++ js/app.js
@@ -0,0 +1,70 @@
+const tagsEl = document.querySelector(".tags"),
+      textArea = document.querySelector("#textarea");
+
+textArea.focus();
+
+textArea.addEventListener("keyup",(e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter'){
+        setTimeout(() => {
+                e.target.value = ''
+        }, 100);
+        randomSelect();
+    }
+})
+
+// 11:48da error bo'ldi
+// 12:03da to'girlandi
+
+function createTags(input) {
+    const tags = input
+    .split(",")
+    .filter(tag => tag.trim() !== "")
+    .map(tag => tag.trim());
+
+    tagsEl.innerHTML = "";
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement("span");
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl)
+    });
+}
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100);
+    }, 100);
+
+
+    setTimeout(() => {
+        clearInterval(interval)
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag)
+        },100)
+    }, times * 100);
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag){
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag){
+    tag.classList.remove('highlight')
+}
